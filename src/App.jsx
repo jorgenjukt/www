f0f0c8d2b3d2ffabbbbb0dc69aab6fa72b21afe0
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-23 16:14:12 +0530

    v.0.1 release

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Navigation from "./components/Navigation";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 function App() {
-  return <>hallo</>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ProductZX9.jsx
@@ -0,0 +1,9 @@
+const ProductZX9 = () => {
+  return (
+    <div>
+      <h2>zx9 - an fast key/value store.</h2>
+    </div>
+  );
+};
+
+export default ProductZX9;
